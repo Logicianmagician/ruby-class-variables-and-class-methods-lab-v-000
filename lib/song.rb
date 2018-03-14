ad973f3c67e7cb562a613b356af28694c9d14ee9
Andrew Kairys <andrewkairys@gmail.com>
2018-03-14 17:02:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,13 @@ class Song
   end
 
   def self.genre_count
-    @@genres.each {|genre| genre_count[genre] ? genre_count[genre] +=1 : genre_count[genre = 1]}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+         genre_count[genre] +=1 
+      else
+         genre_count[genre = 1]
+      end
     genre_count
   end
+  
 end

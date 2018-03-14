4ed4687fcc59c39da8b8c19120fb0d300190ac31
Andrew Kairys <andrewkairys@gmail.com>
2018-03-14 17:03:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,13 +26,14 @@ class Song
   end
 
   def self.genre_count
-    @@genres.each do |genre| 
+    @@genres.each do |genre|
       if genre_count[genre]
-         genre_count[genre] +=1 
+         genre_count[genre] +=1
       else
          genre_count[genre = 1]
       end
+    end
     genre_count
   end
-  
+
 end

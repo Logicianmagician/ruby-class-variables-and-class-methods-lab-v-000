dda99aceb0763f0f9cdde8db615ac4bf94442b91
Andrew Kairys <andrewkairys@gmail.com>
2018-03-14 16:45:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
  attr_accessor :name, :artist, :genre
-  @@count = []
+  @@count = 0
   @@artists = []
   @@genres = []
   def initialize(name, artist, genre)
@@ -9,6 +9,7 @@ class Song
     @@artists << @artist
     @genre = genre
     @@genres << @genre
+    @@count += 1
   end
 
   def count

12d4e18b117073495a0b8e5539f88a6ea81730f8
Andrew Kairys <andrewkairys@gmail.com>
2018-03-14 16:38:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,10 @@ class Song
  attr_accessor :name, :artist, :genre
   @@song_count = 0
 
-  def initialize
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@song_count +=1
   end
 
